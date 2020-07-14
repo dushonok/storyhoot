a45197410644e3f965db60a6fb179b7fc9eac3bd
Nadya Ershova <4466401+dushonok@users.noreply.github.com>
2020-07-14 10:44:40 -0400

    Using instagram-user-feed lib to fetch stories (using one of their examples + an added line with autoload)

--- index.php
+++ index.php
@@ -1,58 +1,20 @@
 <?php
-	$username = "tanya_liberman";
+require __DIR__ . '/../../../../master/vendor/autoload.php';
 
-	$curl = curl_init();
-	curl_setopt_array($curl, [
+use Instagram\Api;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
-		CURLOPT_URL => "https://www.instagram.com/".$username."/?__a=1",
+$cachePool = new FilesystemAdapter('Instagram', 0, __DIR__ . '/../cache');
 
-		CURLOPT_RETURNTRANSFER => 1,
-		CURLINFO_HEADER_OUT => 1,
-		CURLOPT_FOLLOWLOCATION => 1,
-		CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
-		CURLOPT_ENCODING => "",
-		CURLOPT_MAXREDIRS => 10,
-		CURLOPT_TIMEOUT => 0,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "GET",
-		CURLOPT_HTTPHEADER => array(
-			"accept-encoding: gzip, deflate, br; user-agent: PostmanRuntime/7.26.1; postman-token: 87036781-567d-4074-956e-b56e54ab3074"
-		),
+$api = new Api($cachePool);
+$api->login('milawoofdogs', '8hKU3aIWk0NE6QbbwwWlMqjCXYYhrYTs'); // mandatory
 
-		//CURLOPT_COOKIE => "sessionid="
-	]);
+$profile = $api->getProfile('tanya_liberman'); // we need instagram username
+sleep(1);
+$feedStories = $api->getStories($profile->getId());
 
-	$output = curl_exec($curl);
+$stories = $feedStories->getStories();
 
-	$res = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-	print_r("\nRequest status:\n");
-	print_r($res);
-
-	$res = curl_getinfo($curl, CURLINFO_HEADER_OUT);
-	print_r("\nHeaders:\n");
-	print_r($res);
-
-
-	curl_close ($curl);
-
-	print_r("\nServer output:\n");
-	print_r($output);
-
-	$output = json_decode($output,NULL);
-
-	print_r("\nDecoded output:\n");
-	print_r($output);
-
-	if(isset($output->items)) {
-		print_r("\nI'm inside IF!\n");
-		// $last_story_array = array_reverse($output->items);
-		
-		// if(count($last_story_array) > 0) {
-		//   $last_story = $last_story_array[0];
-
-		//   $purchasedToday = time() - $last_story->taken_at ;
-		//   $last_story_at = $purchasedToday/3600;
-		// }
-	}
+print_r($stories);
 
 ?>
